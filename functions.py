aa588cebf93313243d8ea1815c2b36f509e2d14e
Dmitry <novozhilovdmitry93@gmail.com>
2025-02-16 23:51:41 +0500

    add post func for get_wallet and place_order

--- functions.py
+++ functions.py
@@ -3,11 +3,6 @@ import requests
 from datetime import datetime
 
 
-HOST = 'https://api-testnet.bybit.com'
-VERSION = 'v5'
-PRODUCT = 'market'
-
-
 def get_data_from_json_file(full_path):
     """
     :param full_path: полный путь к файлу
@@ -18,11 +13,11 @@ def get_data_from_json_file(full_path):
     return data
 
 
-def get_server_time(host=HOST, version=VERSION, product=PRODUCT):
+def get_server_time(host):
     """
     :return: возвращает такие данные {'timeSecond': '1738692213', 'timeNano': '1738692213900666676'}
     """
-    _bybit_server_time = requests.get(f'{host}/{version}/{product}/time')
+    _bybit_server_time = requests.get(f'{host}/v5/market/time')
     server_times = _bybit_server_time.json()['result']
     return server_times
 
@@ -36,22 +31,27 @@ def from_int_to_date(timestamp: int) -> str:
     return dt
 
 
-def datetime_to_int(date_str: str) -> int:
+def datetime_to_int(date_str, mask):
     """
     :param date_str: 05.02.2025 02:38:00.000000
+    :param mask: "%d.%m.%Y %H:%M:%S.%f"
     :return: Преобразует дату и время в целочисленный Unix timestamp (1738705080000)
     """
-    dt = datetime.strptime(date_str, "%d.%m.%Y %H:%M:%S.%f")  # Преобразуем строку в datetime
-    return int(dt.timestamp() * 1000)
+    if isinstance(date_str, str):
+        dt = datetime.strptime(date_str, mask)  # Преобразуем строку в datetime
+        return int(dt.timestamp() * 1000)
+    elif isinstance(date_str, datetime):
+        return int(date_str.timestamp() * 1000)
 
 
-def get_historical_volatolity(basecoin, period, host=HOST, version=VERSION, product=PRODUCT):
+
+def get_historical_volatolity(basecoin, period, host):
     """
     basecoin код валюты
     period int значение
     :return: показывает волатильность валюты за период
     """
-    _address = f'{host}/{version}/{product}/historical-volatility'
+    _address = f'{host}/v5/market/historical-volatility'
     params = {
         'category': 'option',
         'baseCoin': basecoin.upper(),
@@ -60,3 +60,70 @@ def get_historical_volatolity(basecoin, period, host=HOST, version=VERSION, prod
     data = requests.get(_address, params=params)
     return data.json()
 
+
+def get_wallet_balance(api_key, api_secret, current_time, host):
+    """
+    :param api_key: ключ API, который получили
+    :param api_secret: зашифрованный секретный ключ
+            hmac.new(api_secret.encode(), data_to_sign.encode(), hashlib.sha256).hexdigest()
+    :param current_time: текущее числовое время
+    :param host: host
+    :return: retMsg, result
+    """
+    _url = f'{host}/v5/account/wallet-balance'
+    headers = {
+        'X-BAPI-API-KEY': api_key,
+        'X-BAPI-TIMESTAMP': current_time,
+        'X-BAPI-RECV-WINDOW': '20000',
+        'X-BAPI-SIGN': api_secret,
+    }
+    request = requests.get(_url, headers=headers)
+    try:
+        return request.json()
+    except json.JSONDecodeError:
+        return 'Ошибка! Не удалось получить ответ от сервера. Проверьте HOST'
+
+
+def place_order(api_key, api_secret, current_time, host):
+    _url = f'{host}/v5/order/create'
+    payload = {"category": "linear",
+               "symbol": "ETHUSDT",
+               "isLeverage": 0,
+               "side": "Buy",
+               "orderType": "Limit",
+               "qty": "1",
+               "price": "1000",
+               "triggerPrice": None,
+               "triggerDirection": None,
+               "triggerBy": None,
+               "orderFilter": None,
+               "orderIv": None,
+               "timeInForce": "GTC",
+               "positionIdx": 0,
+               "orderLinkId": "test-xx1",
+               "takeProfit": None,
+               "stopLoss": None,
+               "tpTriggerBy": None,
+               "slTriggerBy": None,
+               "reduceOnly": False,
+               "closeOnTrigger": False,
+               "smpType": None,
+               "mmp": None,
+               "tpslMode": None,
+               "tpLimitPrice": None,
+               "slLimitPrice": None,
+               "tpOrderType": None,
+               "slOrderType": None
+               }
+    json_struct = json.dumps(payload)
+    headers = {
+        'X-BAPI-API-KEY': api_key,
+        'X-BAPI-TIMESTAMP': current_time,
+        'X-BAPI-RECV-WINDOW': '20000',
+        'X-BAPI-SIGN': api_secret,
+    }
+    request = requests.post(_url, headers=headers, data=json_struct)
+    try:
+        return request.json()
+    except json.JSONDecodeError:
+        return 'Ошибка! Не удалось получить ответ от сервера. Проверьте HOST'
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import requests
 import pathlib
+import hmac
+import hashlib
 from datetime import datetime
-from functions import (get_data_from_json_file, get_server_time, from_int_to_date, datetime_to_int,
-                       get_historical_volatolity)
 
+
+from functions import (get_data_from_json_file, get_server_time, from_int_to_date, datetime_to_int,
+                       get_historical_volatolity, get_wallet_balance, place_order)
+
+
+host_testnet = 'https://api-testnet.bybit.com'
+host_demo = 'https://api-demo.bybit.com'
+coin = 'BTC'
+path_to_api_key = pathlib.Path.cwd().joinpath('api_key.json')
+path_to_api_secret = pathlib.Path.cwd().joinpath('api_secret.json')
+X_BAPI_API_KEY = get_data_from_json_file(path_to_api_key)['api_key_test']
+api_secret = get_data_from_json_file(path_to_api_secret)['api_key_test']
 
 # server_time = get_server_time()
-date_from = '01.02.2025 23:00:00'
-date_to = '01.02.2025 23:30:00'
+time = datetime.now()
+cur_time = str(datetime_to_int(time, '%Y.%m.%d %H:%M:%S.%f'))
+data_to_sign = cur_time + X_BAPI_API_KEY + '20000'
+signature = hmac.new(api_secret.encode(), data_to_sign.encode(), hashlib.sha256).hexdigest()
+# print(get_wallet_balance(X_BAPI_API_KEY, signature, cur_time, host_testnet))
+print(place_order(X_BAPI_API_KEY, signature, cur_time, host_demo))
 
-path_to_api_key = pathlib.Path.cwd().joinpath('api_key.json')
-X_BAPI_API_KEY = get_data_from_json_file(path_to_api_key)['api_key_test']
-X_BAPI_TIMESTAMP = 'Временная метка UTC в миллисекундах' # что это такое?
-X_BAPI_SIGN = 'подпись, полученная из параметров запроса' # что это такое?
-X_Referer = 'заголовок только для пользователей брокера' # что это такое?
-
-coin = 'BTC'
-start = 1735671600000 # 01.01.2025 00:00:00.000000
-end = 1736017200000 # 05.01.2025 00:00:00.000000
